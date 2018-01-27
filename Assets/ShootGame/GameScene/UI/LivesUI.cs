0e938d949332c47b94d59efadbc2c6247f516ab9
Adam Proctor <adam@uken.com>
2018-01-27 13:57:30 -0500

    lives

---
+++ Assets/ShootGame/GameScene/UI/LivesUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesUI : MonoBehaviour {
+
+	public DeathGod deathGod;
+
+	public Text livesLabel;
+
+	private int curLives = 1;
+
+	void Update() {
+		if (deathGod.currentLives != curLives) {
+			curLives = deathGod.currentLives;
+			this.livesLabel.text = curLives.ToString();
+		}
+	}
+}
